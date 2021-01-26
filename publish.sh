efe622b188004c89ddb63a8ddbf3301c2e728401
Tom Donohue <monodot@gmail.com>
2021-01-26 12:41:00 +0000

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#
+# Publishes this Jekyll static site to a directory
+# Usage: ./publish.sh <env> <destPath>
+
+bundle config set path 'vendor/bundle'
+bundle install
+
+echo "Building site..."
+JEKYLL_ENV=${1:-production} bundle exec jekyll build -d ${2:-/var/www/kb.tomd.xyz/public_html}
+echo "Site build complete."
