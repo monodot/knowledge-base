4af3528ecd291301dd6eda1c1350f467912f1bd4
Tom Donohue <monodot@gmail.com>
2021-01-26 14:35:09 +0000

    Do not install on every build

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@
 # Publishes this Jekyll static site to a directory
 # Usage: ./publish.sh <env> <destPath>
 
-bundle config set path 'vendor/bundle'
-bundle install
+#bundle config set path 'vendor/bundle'
+#bundle install
 
 echo "Building site..."
 JEKYLL_ENV=${1:-production} bundle exec jekyll build -d ${2:-/var/www/kb.tomd.xyz/public_html}
