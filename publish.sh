e8ea66c73949be43f634dabe51a03db85e81c092
Tom Donohue <monodot@gmail.com>
2021-01-26 19:21:48 +0000

    Update publish

--- publish.sh
+++ publish.sh
@@ -1,11 +1,20 @@
 #!/bin/bash -l
 #
-# Publishes this Jekyll static site to a directory
-# Usage: ./publish.sh <env> <destPath>
+# Publishes this Jekyll static site
+# Usage: ./publish.sh <env> <bundleCacheLocation>
 
-#bundle config set path 'vendor/bundle'
-#bundle install
+JEKYLL_ENV=${1:-production}
+BUNDLE_APP_CONFIG=${2:-/usr/local/bundle}
 
 echo "Building site..."
-JEKYLL_ENV=${1:-production} bundle exec jekyll build -d ${2:-/var/www/kb.tomd.xyz/public_html}
+
+# Run the bundle install and jekyll build in a container
+# The ruby container uses /usr/local/bundle as a local artifact location
+podman run --rm \
+    -v "$PWD":/usr/src/site \
+    -v ${BUNDLE_APP_CONFIG}:/usr/local/bundle \
+    -w /usr/src/site \
+    -e JEKYLL_ENV=${JEKYLL_ENV} \
+    docker.io/library/ruby:2.7 /bin/bash -c "bundle install && bundle exec jekyll build"
+
 echo "Site build complete."
